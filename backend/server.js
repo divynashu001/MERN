c5a0081193e8bd27e67ca97d2556c68e868defd8
divynashu001 <divyanshudhiman41@gmail.com>
2025-02-14 01:56:16 +0530

    update in cors

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ dotenv.config();
 const router = require("./routes/userRoute");
 const cors = require("cors");
 
-// app.use(cors({
-//   origin: "http://localhost:5173", // Allow requests from this origin
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Allow these HTTP methods
-//   credentials: true, // Allow cookies and credentials
-// }));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // Allow requests from this origin
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Allow these HTTP methods
+  credentials: true, // Allow cookies and credentials
+}));
+// app.use(cors());
 app.use("/user", router);
 
 mongoose
